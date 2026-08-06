84d8005b111140aa523656c171ac93a7c0cb286e
Anton Shabat <anton.shabat@gmail.com>
2026-08-06 21:28:10 +0300

    first ssh config

---
+++ modules/ssh/ssh.nix
@@ -0,0 +1,24 @@
+{lib, ...} : {
+  den.aspects.ssh = {
+    nixos = {user, ...} : {
+      services.openssh = {
+        enable = true;
+	openFirewall = true;
+	settings = {
+	  PasswordAuthentication = false;
+	  KbdInteractiveAuthentication = false;
+	  PermitRootLogin = "no";
+	  AllowUsers = [ user.userName ];
+	  MaxAuthTries = 3;
+	  PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+	};
+      };
+    };
+
+    user = {
+      openssh.authorizedKeys.keyFiles = let
+        is-pub-key = path: lib.hasSuffix (builtins.baseNameOf path) ".pub";
+      in builtins.filter is-pub-key (lib.filesystem.listFilesRecursive .);
+    };
+  };
+}
