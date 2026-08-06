6742a43466519cff8c816b397d9a54b2b8d82c96
Anton Shabat <anton.shabat@gmail.com>
2026-08-06 23:17:11 +0300

    trying to persist host ssh keys

--- modules/ssh/ssh.nix
+++ modules/ssh/ssh.nix
@@ -22,6 +22,7 @@
       builtins.filter is-pub-key (lib.filesystem.listFilesRecursive ./.);
     };
 
+    persist.files = builtins.map (key: "/etc/ssh/ssh_host_" + key) [ "ed25519_key" "ed25519_key.pub" "rsa_key" "rsa_key.pub" ];
     persist.home.files = [ ".ssh/id_ed25519" ".ssh/id_ed25519.pub" ];
   };
 }
