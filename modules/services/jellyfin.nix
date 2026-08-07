8a61ff4eab5fd35006267a76520d1a8480cd0404
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 16:30:55 +0300

    add jellyfin

---
+++ modules/services/jellyfin.nix
@@ -0,0 +1,20 @@
+{den, ...} : {
+  den.aspects.jellyfin = {
+    includes = [ den.aspects.caddy ];
+  
+    nixos = {
+      services.jellyfin = {
+        enable = true;
+      };
+    };
+
+    persist.directories = [
+      "/var/cache/jellyfin"
+      "/var/lib/jellyfin"
+    ];
+
+    caddy-config."hierophant.antosha.xyz" = ''
+    reverse_proxy 127.0.0.1:8096
+    '';
+  };
+}
