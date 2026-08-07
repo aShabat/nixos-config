60aac60ffd8ff562c5d2130af8b1169ab8aaaf15
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 10:14:35 +0300

    fixup! add run0 and run0-sudo-shim

--- modules/shell/sudo.nix
+++ modules/shell/sudo.nix
@@ -1,9 +1,16 @@
 {
   den.aspects.sudo = {
     nixos = {
-      security.run0 = {
-        enable = true;
-	sudo-shim.enable = true;
+      security = {
+        sudo.enable = false;
+        run0 = {
+          enable = true;
+	  sudo-shim.enable = true;
+	  persistentAuth = {
+	    enable = true;
+	    enableRemote = true;
+	  };
+	};
       };
     };
   };
