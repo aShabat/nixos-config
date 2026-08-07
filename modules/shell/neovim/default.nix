0d87357fe219e8446bcb73069a6a019ad2a7f1d9
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 22:30:33 +0300

    nvim symlink config

--- modules/shell/neovim/default.nix
+++ modules/shell/neovim/default.nix
@@ -5,7 +5,7 @@
         neovim-unwrapped
       ];
 
-      home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ./.;
+      home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/shell/neovim";
     };
   };
 }
