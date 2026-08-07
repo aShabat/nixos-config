cbcf422fc0ed489a0d7456f224c405d3f7a36ce0
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 13:01:24 +0300

    add cloudflared tunnel credential

--- modules/services/cloudflared/cloudflared.nix
+++ modules/services/cloudflared/cloudflared.nix
@@ -4,6 +4,11 @@
       services.cloudflared = {
         enable = true;
 	certificateFile = ./cert.pem.secret;
+	tunnels = {
+	  "3ee421dd-2d64-4222-bec7-d6023865e11c" = {
+	    credentialsFile = ./3ee421dd-2d64-4222-bec7-d6023865e11c.json;
+	  };
+	};
       };
     };
   };
