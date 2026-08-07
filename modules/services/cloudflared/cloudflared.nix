8e92055b2d73b2d12870b12ae6b26700d5f81076
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 16:01:57 +0300

    fixup! cloudflared tunnel defaults to localhost:80

--- modules/services/cloudflared/cloudflared.nix
+++ modules/services/cloudflared/cloudflared.nix
@@ -9,7 +9,7 @@
 	tunnels = {
 	  "3ee421dd-2d64-4222-bec7-d6023865e11c" = lib.mkIf (match-host "hierophant") {
 	    credentialsFile = ./3ee421dd-2d64-4222-bec7-d6023865e11c.json.secret;
-	    default = "https://localhost:80";
+	    default = "http://localhost:80";
 	  };
 	};
       };
