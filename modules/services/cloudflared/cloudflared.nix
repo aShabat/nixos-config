30991a16cb3e33b5120f5ba6ba35e65f977bef55
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 14:51:35 +0300

    cloudflared match tunnel to host

--- modules/services/cloudflared/cloudflared.nix
+++ modules/services/cloudflared/cloudflared.nix
@@ -1,12 +1,15 @@
-{
+{lib, ...} : {
   den.aspects.cloudflared = {
-    nixos = {
+    nixos = {host, ...} : let
+      match-host =  hostName : host.hostName == hostName;
+    in {
       services.cloudflared = {
         enable = true;
 	certificateFile = ./cert.pem.secret;
 	tunnels = {
-	  "3ee421dd-2d64-4222-bec7-d6023865e11c" = {
-	    credentialsFile = ./3ee421dd-2d64-4222-bec7-d6023865e11c.json;
+	  "3ee421dd-2d64-4222-bec7-d6023865e11c" = lib.mkIf (match-host "hierophant") {
+	    credentialsFile = ./3ee421dd-2d64-4222-bec7-d6023865e11c.json.secret;
+	    default = "hello_world";
 	  };
 	};
       };
