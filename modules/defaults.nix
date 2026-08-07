b29262e093501ee2c52c90dd890b615465785a7f
Anton Shabat <anton.shabat@gmail.com>
2026-08-07 17:06:06 +0300

    boot aspect

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -2,7 +2,7 @@
 {
   den.default.includes = with den.batteries; [ define-user hostname ];
 
-  # den.default.nixos.system.stateVersion = "26.05";
+  den.default.nixos.system.stateVersion = "26.05";
   den.default.homeManager.home.stateVersion = "26.05";
 
   # enable hm by default
--- modules/hierophant.nix
+++ modules/hierophant.nix
@@ -1,6 +1,6 @@
 {den, ...} : {
   den.aspects.hierophant = {
-    includes = with den.aspects; [ network-manager impermanence ssh nix cloudflared caddy jellyfin close-lid ];
+    includes = with den.aspects; [ network-manager impermanence ssh nix cloudflared caddy jellyfin close-lid boot ];
 
     nixos =
       { pkgs, ... }:
