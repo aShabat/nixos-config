b82ed569698b51290fbbb02b29b57d3575e81a0c
Anton Shabat <anton.shabat@gmail.com>
2026-08-08 09:32:34 +0300

    nvim way to add runtime dependencies

--- modules/shell/neovim/default.nix
+++ modules/shell/neovim/default.nix
@@ -1,8 +1,20 @@
 {
   den.aspects.neovim = {
-    homeManager = {pkgs, config, ...} : {
-      home.packages = with pkgs; [
-        neovim-unwrapped
+    homeManager = {pkgs, config, ...} : let
+      utils = with pkgs; [];
+      languages = with pkgs; [];
+      language-servers = with pkgs; [];
+      formatters = with pkgs; [];
+      my-nvim = pkgs.writeShellApplication {
+        name = "nvim";
+	runtimeInputs = utils ++ languages ++ language-servers ++ formatters;
+	text = ''
+	  exec ${pkgs.neovim-unwrapped}/bin/nvim "$@"
+	'';
+      };
+    in {
+      home.packages = [
+        my-nvim
       ];
 
       home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/shell/neovim";
