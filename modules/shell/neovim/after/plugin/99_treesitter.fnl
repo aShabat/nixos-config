b0c68a3f38db05076ab8cb4ec7229f42fd199493
Anton Shabat <anton.shabat@gmail.com>
2026-08-09 00:04:51 +0300

    nvim added treesitter-context and rainbow-delimiters

--- modules/shell/neovim/after/plugin/99_treesitter.fnl
+++ modules/shell/neovim/after/plugin/99_treesitter.fnl
@@ -1,9 +1,12 @@
 ;nix-pkgs:gnutar:curl:tree-sitter:gcc
-(vim.pack.add [{:src "https://github.com/nvim-treesitter/nvim-treesitter"}])
+(vim.pack.add [{:src "https://github.com/nvim-treesitter/nvim-treesitter"}
+               {:src "https://github.com/nvim-treesitter/nvim-treesitter-context"}
+               {:src "https://github.com/hiphish/rainbow-delimiters.nvim"}])
+
 (local {: mapcat} (require :nfnl.core))
 (local nvim-treesitter (require :nvim-treesitter))
 
-(nvim-treesitter.install [:nix :lua :fennel] {})
+(nvim-treesitter.install [:nix :lua :fennel :c :vim :vimdoc :query] {})
 
 (local group (vim.api.nvim_create_augroup :treesitter {}))
 (let [languages (nvim-treesitter.get_installed)
@@ -14,3 +17,13 @@
                                             (set vim.bo.indentexpr
                                                  "v:lua()require'nvim-treesitter'.indentexpr()"))
                                 : group}))
+
+((. (require :treesitter-context) :setup) {:separator "."})
+
+(vim.keymap.set :n :gC
+                (fn []
+                  (let [tc (require :treesitter-context)]
+                    (tc.go_to_context vim.v.count1)))
+                {:silent true})
+
+{}
--- modules/shell/neovim/after/plugin/99_treesitter.lua
+++ modules/shell/neovim/after/plugin/99_treesitter.lua
@@ -1,15 +1,24 @@
 -- [nfnl] after/plugin/99_treesitter.fnl
-vim.pack.add({{src = "https://github.com/nvim-treesitter/nvim-treesitter"}})
+vim.pack.add({{src = "https://github.com/nvim-treesitter/nvim-treesitter"}, {src = "https://github.com/nvim-treesitter/nvim-treesitter-context"}, {src = "https://github.com/hiphish/rainbow-delimiters.nvim"}})
 local _local_1_ = require("nfnl.core")
 local mapcat = _local_1_.mapcat
 local nvim_treesitter = require("nvim-treesitter")
-nvim_treesitter.install({"nix", "lua", "fennel"}, {})
+nvim_treesitter.install({"nix", "lua", "fennel", "c", "vim", "vimdoc", "query"}, {})
 local group = vim.api.nvim_create_augroup("treesitter", {})
-local languages = nvim_treesitter.get_installed()
-local filetypes = mapcat(vim.treesitter.language.get_filetypes, languages)
-local function _2_()
-  vim.treesitter.start()
-  vim.bo.indentexpr = "v:lua()require'nvim-treesitter'.indentexpr()"
-  return nil
+do
+  local languages = nvim_treesitter.get_installed()
+  local filetypes = mapcat(vim.treesitter.language.get_filetypes, languages)
+  local function _2_()
+    vim.treesitter.start()
+    vim.bo.indentexpr = "v:lua()require'nvim-treesitter'.indentexpr()"
+    return nil
+  end
+  vim.api.nvim_create_autocmd("FileType", {pattern = filetypes, callback = _2_, group = group})
 end
-return vim.api.nvim_create_autocmd("FileType", {pattern = filetypes, callback = _2_, group = group})
+require("treesitter-context").setup({separator = "."})
+local function _3_()
+  local tc = require("treesitter-context")
+  return tc.go_to_context(vim.v.count1)
+end
+vim.keymap.set("n", "gC", _3_, {silent = true})
+return {}
