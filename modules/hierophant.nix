c1c46c4bc1412d87b6c8cc4e44ae5c27dcea5da5
Anton Shabat <anton.shabat@gmail.com>
2026-08-09 10:46:23 +0300

    removed unneeded includet in hierophant

--- modules/hierophant.nix
+++ modules/hierophant.nix
@@ -1,6 +1,6 @@
 {den, ...}: {
   den.aspects.hierophant = {
-    includes = with den.aspects; [network-manager impermanence ssh nix cloudflared caddy jellyfin close-lid boot];
+    includes = with den.aspects; [network-manager impermanence ssh nix jellyfin close-lid boot];
 
     nixos = {
       config,
