b116c0797b7c219e61969d2e206165a785761f33
Anton Shabat <anton.shabat@gmail.com>
2026-08-19 12:04:05 +0300

    nvim add nixd lsp

---
+++ modules/shell/neovim/after/lsp/nixd.fnl
@@ -0,0 +1,2 @@
+;nix-pkgs:nixd
+{:cmd [:nixd] :filetypes [:nix] :root_markers [:.git :flake.nix] :settings {}}
---
+++ modules/shell/neovim/after/lsp/nixd.lua
@@ -0,0 +1,2 @@
+-- [nfnl] after/lsp/nixd.fnl
+return {cmd = {"nixd"}, filetypes = {"nix"}, root_markers = {".git", "flake.nix"}, settings = {}}
