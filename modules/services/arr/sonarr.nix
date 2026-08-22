34c9e39374a30f0e476b39b2f3808a7a22e4f30a
Anton Shabat <anton.shabat@gmail.com>
2026-08-22 14:34:29 +0300

    add sonarr to hierophant

---
+++ modules/services/arr/sonarr.nix
@@ -0,0 +1,19 @@
+{
+  den.aspects.sonarr = {
+    nixos = {
+      services.sonarr = {
+        enable = true;
+
+        settings = {
+          update.automatically = true;
+        };
+      };
+
+      caddy-configs."http://sonarr.antosha.xyz" = ''
+        reverse_proxy 127.0.0.1:8989
+      '';
+
+      persist.directories = ["/var/lib/sonarr"];
+    };
+  };
+}
