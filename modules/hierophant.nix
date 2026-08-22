b0aefc908e897f9cbb94aa9614b7c4353d3561fa
Anton Shabat <anton.shabat@gmail.com>
2026-08-22 12:43:27 +0300

    hierophant add hdd to hardware config

--- modules/hierophant.nix
+++ modules/hierophant.nix
@@ -13,7 +13,7 @@
         (modulesPath + "/installer/scan/not-detected.nix")
       ];
 
-      boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+      boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "uas" "usb_storage" "sd_mod"];
       boot.initrd.kernelModules = [];
       boot.kernelModules = ["kvm-intel"];
       boot.extraModulePackages = [];
@@ -36,76 +36,15 @@
         options = ["subvol=nix"];
       };
 
-      fileSystems."/var/lib/nixos" = {
-        device = "/nix/persistent/var/lib/nixos";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/etc/nixos" = {
-        device = "/nix/persistent/etc/nixos";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.gnupg" = {
-        device = "/nix/persistent/home/anton/.gnupg";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/etc/NetworkManager/system-connections" = {
-        device = "/nix/persistent/etc/NetworkManager/system-connections";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.local/share/keyrings" = {
-        device = "/nix/persistent/home/anton/.local/share/keyrings";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.local/share/nvim" = {
-        device = "/nix/persistent/home/anton/.local/share/nvim";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.local/share/zoxide" = {
-        device = "/nix/persistent/home/anton/.local/share/zoxide";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.local/state/comma" = {
-        device = "/nix/persistent/home/anton/.local/state/comma";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/home/anton/.local/state/nvim" = {
-        device = "/nix/persistent/home/anton/.local/state/nvim";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/var/lob/journal" = {
-        device = "/nix/persistent/var/lob/journal";
-        fsType = "none";
-        options = ["bind"];
-      };
-
-      fileSystems."/var/lib/systemd/coredump" = {
-        device = "/nix/persistent/var/lib/systemd/coredump";
-        fsType = "none";
-        options = ["bind"];
-      };
-
       fileSystems."/boot" = {
         device = "/dev/disk/by-uuid/59EE-96F0";
         fsType = "vfat";
         options = ["fmask=0022" "dmask=0022"];
+      };
+
+      fileSystems."/hdd" = {
+        device = "/dev/disk/by-uuid/a0612e41-7076-418d-9cd9-61ece503eac8";
+        fsType = "ext4";
       };
 
       swapDevices = [];
